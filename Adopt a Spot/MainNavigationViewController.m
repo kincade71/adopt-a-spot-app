3a0c747af60223a24b2f9696aaf4c9856ca52c82
kincade71 <kincade71@gmail.com>
2015-10-23 20:30:15 -0400

    Added height to uitableview to fill view for iPhone 6.

--- Adopt a Spot/MainNavigationViewController.m
+++ Adopt a Spot/MainNavigationViewController.m
@@ -37,7 +37,7 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    tableView.rowHeight = 300;
+    tableView.rowHeight = 302;
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     NSString *imagename = [self.data objectAtIndex:indexPath.row];
     
